1dcaee96543e3722402ca6c60d20c3572fc08403
DRC-PJudson <35580463+DRC-PJudson@users.noreply.github.com>
2018-03-14 15:00:31 -0500

    Add post todos route and refactor mongoose

---
+++ server/models/todo.js
@@ -0,0 +1,35 @@
+
+
+var mongoose = require('mongoose');
+
+var Todo = mongoose.model('Todo', {
+  text: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number,
+    default: null
+  }
+});
+
+// var newTodo = new Todo({
+//   text: "Dancing Machine"
+// });
+//
+// newTodo.save().then((doc) => {
+//
+//   console.log('Save Todo');
+//   console.log(JSON.stringify(doc, undefined, 2));
+//
+// }, (e) => {
+//   console.log('Unable to save todo', e);
+// });
+
+module.exports = {Todo};
